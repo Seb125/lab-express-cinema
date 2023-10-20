3c215b6d03e1cd3c3dea32b31be1cade156d631f
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2023-10-20 10:16:05 +0200

    update and delete

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -27,6 +27,19 @@ const getMovieDetails = async (req, res) => {
 }
 
 
+const createMovie = async (req, res) => {
+    const movie =  req.body;
+
+    
+
+    Movie.create(movie);
+
+    res.redirect('/');
+}
+
+
+
+
 
 router.get("/", (req, res) => {
 
@@ -35,9 +48,90 @@ router.get("/", (req, res) => {
 })
 
 
+router.get("/create", (req, res) => {
+
+    res.render("create-movie")
+
+})
+
+router.post("/create", (req, res) => {
+
+    createMovie(req, res);
+
+})
+
 router.get("/:id", (req, res) => {
   
     getMovieDetails(req, res);
+})
+
+
+
+router.get("/:id/update", (req, res) => {
+
+    const updateMovie = async (req, res) => {
+        try {
+            const movieId = req.params.id;
+            
+            const details = await Movie.findById(movieId);
+            
+            res.render('update-movie', {details: details})
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+
+
+    updateMovie(req, res);
+})
+
+
+router.post("/:id/update", (req, res) => {
+    const updateMovie = async (req, res) => {
+        try {
+            const movieId = req.params.id;
+            console.log(movieId)
+            const newData = req.body;
+
+            const formattedData = {
+                title: newData.title,
+                director: newData.director,
+                stars: newData.stars.split(','),
+                image: newData.image,
+                description: newData.description,
+                showtimes: newData.showtimes.split(',')
+            }
+            console.log(formattedData)
+            const updatedMovie = await Movie.findByIdAndUpdate(movieId, formattedData);
+            
+            res.redirect('/movies')
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+
+
+    updateMovie(req, res);
+})
+
+router.get("/:id/delete", (req, res) => {
+    const deleteMovie = async (req, res) => {
+        
+        try {
+            const movieId = req.params.id;
+
+            const deletedMovie = await Movie.findByIdAndDelete(movieId);
+
+            res.redirect('/movies');
+        } catch(err) {
+            console.log(err)
+        }
+
+    }
+
+    deleteMovie(req, res);
 })
 
 
